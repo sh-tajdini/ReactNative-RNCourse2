795a508e67ef0c1eb9a9c8f7fb5f9f96b47397c3
Shervin Tajdini <shervin.taj@gmail.com>
2023-12-12 15:56:12 -0500

    deleting and updating expenses

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -3,8 +3,11 @@ import { View,StyleSheet } from "react-native";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import Button from "../components/UI/Button";
+import { useContext } from "react";
+import { ExpensesContext } from "../store/expenses-context";
 
 function ManageExpense({route,navigation}) {
+    const expensesCtx = useContext(ExpensesContext);
     const editedExpenseId = route.params?.expenseId;
     const isEditing = !!editedExpenseId;
 
@@ -16,7 +19,9 @@ function ManageExpense({route,navigation}) {
     }, [navigation, isEditing]);
 
     function deleteExpenseHandler(){
+        expensesCtx.deleteExpense(editedExpenseId);
         navigation.goBack();
+
     }
 
     function cancelHandler(){
@@ -24,6 +29,20 @@ function ManageExpense({route,navigation}) {
     }
 
     function confirmHandler(){   
+        if(isEditing){
+            expensesCtx.updateExpense(editedExpenseId,{
+                description:'Test!!!!',
+                amount:29.99,
+                date: new Date('2022-05-20'),
+            });
+        }
+        else{
+            expensesCtx.addExpense({
+                description:'Test',
+                amount:19.99,
+                date: new Date('2022-05-19'),
+            });
+        }
         navigation.goBack();
     }
 
