d361f6c06d05e92fb4bdba300d337dabe640d183
Shervin Tajdini <shervin.taj@gmail.com>
2023-12-12 14:45:35 -0500

    Managing App-wide State With Context

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,69 +3,7 @@ import ExpensesSummary from './ExpensesSummary';
 import ExpensesList from './ExpensesList';
 import { GlobalStyles } from '../../constants/styles';
 
-const DUMMY_EXPENSES = [
-    {
-        id: 'e1', 
-        description: 'A pair of shoes',
-        amount: 59.99, 
-        date: new Date('2021-12-19')
-    },
-    {
-        id: 'e2', 
-        description: 'A pair of trousers',
-        amount: 89.29, 
-        date: new Date('2022-01-05')
-    },
-    {
-        id: 'e3', 
-        description: 'Some bananas',
-        amount: 5.99, 
-        date: new Date('2021-12-1')
-    },
-    {
-        id: 'e4', 
-        description: 'A book',
-        amount: 14.99, 
-        date: new Date('2021-02-19')
-        },
-        {
-        id: 'e5', 
-        description: 'Another book',
-        amount: 18.59, 
-        date: new Date('2021-02-19')
-            },
-            {
-                id: 'e6', 
-                description: 'A pair of shoes',
-                amount: 59.99, 
-                date: new Date('2021-12-19')
-            },
-            {
-                id: 'e7', 
-                description: 'A pair of trousers',
-                amount: 89.29, 
-                date: new Date('2022-01-05')
-            },
-            {
-                id: 'e8', 
-                description: 'Some bananas',
-                amount: 5.99, 
-                date: new Date('2021-12-1')
-            },
-            {
-                id: 'e9', 
-                description: 'A book',
-                amount: 14.99, 
-                date: new Date('2021-02-19')
-                },
-                {
-                id: 'e10', 
-                description: 'Another book',
-                amount: 18.59, 
-                date: new Date('2021-02-19')
-                    },
 
-    ];
 //
 function ExpensesOutput({expenses,expensesPeriod}) {
   return (
