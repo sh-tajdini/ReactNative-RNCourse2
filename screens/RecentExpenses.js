baad0d192091affa470b527b01ee9fb2dd9dab16
Shervin Tajdini <shervin.taj@gmail.com>
2023-12-19 15:24:08 -0500

    fetching backend data (get request)

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,12 +1,20 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
+import { fetchExpenses } from "../util/http";
 
 
 
 function RecentExpenses() {
     const expensesCtx = useContext(ExpensesContext);
+
+    useEffect(() => {
+        async function getExpenses() {
+            const expenses = await fetchExpenses();
+        }
+        getExpenses();
+    }, []);
     
     const recentExpenses = expensesCtx.expenses.filter((expense) => {
         const today = new Date();
@@ -16,10 +24,12 @@ function RecentExpenses() {
         return (expense.date >= date7DaysAgo) && (expense.date <= today);
         
     });
-    return <ExpensesOutput  
+    return (
+    <ExpensesOutput  
     expenses= {recentExpenses} 
     expensesPeriod="Last 7 Days"
     fallBackText="No expenses registered in the last 7 days."
-    />;
+    />
+    );
     }
     export default RecentExpenses;
