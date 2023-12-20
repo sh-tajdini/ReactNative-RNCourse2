91487342f412e27601e6013904ebfdcafe12a6e4
Shervin Tajdini <shervin.taj@gmail.com>
2023-12-20 16:06:56 -0500

    clean code

--- util/http.js
+++ util/http.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const BACKEND_URL = 'https://react-native-course-a4ae6-default-rtdb.firebaseio.com';
+
 export async function storeExpense(expenseData) {
    const response = await axios.post(
     BACKEND_URL+'/expenses.json',
