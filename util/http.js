22faf4e8c7169d121b06d81ca25c2f795d3095e5
Shervin Tajdini <shervin.taj@gmail.com>
2023-12-20 12:35:19 -0500

    using response data from post requests

--- util/http.js
+++ util/http.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
 const BACKEND_URL = 'https://react-native-course-a4ae6-default-rtdb.firebaseio.com';
-export function storeExpense(expenseData) {
-   axios.post(
+export async function storeExpense(expenseData) {
+   const response = await axios.post(
     BACKEND_URL+'/expenses.json',
     expenseData
     );
+     const id = response.data.name;
+        return id;
+
 }
 
 export async function fetchExpenses() {
